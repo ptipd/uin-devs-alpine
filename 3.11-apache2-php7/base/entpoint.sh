4ae6c5a09a009a8df16595660c22ee07bd08feea
Lukluk Muhammad <luxmile@uin-malang.ac.id>
2020-01-06 11:19:52 +0700

    + make dir /app/html

--- 3.11-apache2-php7/base/entpoint.sh
+++ 3.11-apache2-php7/base/entpoint.sh
@@ -3,5 +3,9 @@ set -e
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /run/apache2/httpd.pid
+[ -d /app/html ] || {
+	mkdir /app/html
+	chown -R apache:www-data /app/html
+}
 
 exec httpd -DFOREGROUND -f /etc/apache2/httpd.conf "$@"
