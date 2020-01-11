2bbacf1dd3188e05a40c7c009bddb9129310887f
Lukluk Muhammad <luxmile@gmail.com>
2020-01-11 18:56:03 +0700

    + make web job a bit simpler

--- 3.11-apache2-php7/base/entpoint.sh
+++ 3.11-apache2-php7/base/entpoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 set -e
-
-# Apache gets grumpy about PID files pre-existing
 rm -f /run/apache2/httpd.pid
-[ -d /app/html ] || {
-	mkdir /app/html
-	chown -R apache:www-data /app/html
-}
-
 exec httpd -DFOREGROUND -f /etc/apache2/httpd.conf "$@"
